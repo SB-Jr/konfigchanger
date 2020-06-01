b4182e5c818b38417022826160fb4dc375d9841f
SB-Jr <shrijitbasak@gmail.com>
2020-06-01 12:56:51 +0530

    Bakup basic functionality implemented

--- build/lib/konfchanger.py
+++ build/lib/konfchanger.py
@@ -1,38 +1,50 @@
 import click
 import os
 import shutil
+import json
 
 home_path = os.getenv('HOME')
-CONTEXT_DEFAULTS = dict(
-    default_map = {
-        'config-path': os.path.join(home_path, '.konfchanger'),
-        'config-list-path':  '.backup_locations'
-        }
-)
+config = '.konfchanger_default_config'
 
-@click.group(context_settings=CONTEXT_DEFAULTS)
+
+@click.group()
 @click.pass_context
-def konfchanger():
+@click.option('-c', '--config', 'config', default=config, type=click.Path())
+def konfchanger(ctx, config):
     '''This is a tool to backup/restore KDE configuration and styles.'''
-    with open(config) as cfg:
-        ctx.default_map = cfg.read()
+    click.echo('Checking for config file')
+    try:
+        ctx.ensure_object(dict)
+        with open(config, 'r') as cfg:
+            ctx.default_map = json.load(cfg)
+    except:
+        click.echo('config file doesnt not exist at '+config+'\n Either add a config file or pass a <path-to-config-file> using "-c" or "--config" flag')
+    else:
+        click.echo('Config file Found!! \n value = ' + str(ctx.default_map))
     pass
 
-
-@click.command()
-@click.option('-c', '--config-path','config_path', type=click.Path())
+@konfchanger.command()
+@click.option('-c', '--config-path', 'config_path', type=click.Path())
 @click.option('-cl', '--config-list-path', 'config_list_path', type=click.Path())
 @click.option('--name', required=True, prompt='Please give a name to the current configuration backup!\nNOTE: Space in the name will be converted into "_"(underscore)')
 @click.pass_context
 def backup(ctx, config_path, config_list_path, name):
     '''Backup current configuration'''
+    if not config_list_path:
+        config_list_path = ctx.parent.default_map['config_list_path']
     check_config_file(ctx, config_list_path)
+
+    if not config_path:
+        config_path = ctx.parent.default_map['config_path']
+
     if not os.path.isdir(config_path):
-        click.echo('No backup folder found...\nCreating a backup folder at '+ config_path)
+        click.echo(
+            'No backup folder found...\nCreating a backup folder at ' + config_path)
         try:
             os.mkdir(config_path)
         except error:
-            click.echo('Could not create directory for config backup at '+config_path)
+            click.echo(
+                'Could not create directory for config backup at ' + config_path)
             click.echo(error)
             return
     fixed_name = name.replace(' ', '_')
@@ -43,31 +55,33 @@ def backup(ctx, config_path, config_list_path, name):
         configuration_exists = True
     try:
         if configuration_exists:
-            click.confirm('Do you want to overwrite the exisiting configuration backup?', abort=True)
+            click.confirm(
+                'Do you want to overwrite the exisiting configuration backup?', abort=True)
             shutil.rmtree(backup_location)
         os.mkdir(backup_location)
     except error:
-        click.echo('Error creating backup folder at '+ backup_location)
+        click.echo('Error creating backup folder at ' + backup_location)
         click.echo(error)
     else:
-        click.echo(name + ' Backup complete, with the folder name as '+fixed_name)
+        click.echo(
+            name + ' Backup complete, with the folder name as ' + fixed_name)
 
 
-@click.command()
+@konfchanger.command()
 @click.option('--name', required=True, prompt='Please enter the name of the configuration that you want to apply')
 def apply(name):
     '''Apply a backed-up configuration'''
-    #send kwin reconfigure signal
+    # send kwin reconfigure signal
     click.echo(name + ' ---- Applied')
 
 
-@click.command()
+@konfchanger.command()
 def list():
     '''List all available backed up configurations'''
     click.echo('These are the backed up configurations available')
 
 
-@click.command()
+@konfchanger.command()
 @click.confirmation_option()
 def delete_configuration_backup():
     '''Delete a backed-up configuration'''
@@ -76,11 +90,12 @@ def delete_configuration_backup():
 
 def check_config_file(ctx, config_list_path):
     if not os.path.isfile(config_list_path):
-        click.echo('Configuration List not provided, so nothing will be backed up.\n Please create a '+ config_list_path + ' file or provide a new location using the "-cl" flag' )
+        click.echo('Configuration List not provided, so nothing will be backed up.\n Please create a ' +
+                   config_list_path + ' file or provide a new location using the "-cl" flag')
         ctx.abort()
 
 
-konfchanger.add_command(backup)
-konfchanger.add_command(apply)
-konfchanger.add_command(delete_configuration_backup)
-konfchanger.add_command(list)
+# konfchanger.add_command(backup)
+# konfchanger.add_command(apply)
+# konfchanger.add_command(delete_configuration_backup)
+# konfchanger.add_command(list)
