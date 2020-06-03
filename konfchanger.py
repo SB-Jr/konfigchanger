f0a505ab5fc4f85de1c99307a2ec8d5189f6b66d
SB-Jr <shrijitbasak@gmail.com>
2020-06-03 08:48:04 +0530

    Added return error code and TODOs

--- konfchanger.py
+++ konfchanger.py
@@ -14,10 +14,11 @@ from konfchanger_utils import Utils
 
 utils = Utils()
 
+
 @click.group()
 @click.pass_context
 def konfchanger(ctx):
-    '''This is a tool to backup/restore KDE configuration and styles.'''
+    """This is a tool to backup/restore KDE configuration and styles."""
 
     if utils is None:
         ctx.abort()
@@ -26,43 +27,55 @@ def konfchanger(ctx):
     if (ctx.invoked_subcommand != 'init') and (not store_present):
         utils.logger.info('Please run "init" command for the first time using this tool')
         ctx.abort()
+        return 1
+    return 0
 
 
 @konfchanger.command('init')
-@click.option('-v', '--verbose', is_flag=True,  callback=utils.enable_verbose)
+@click.option('-v', '--verbose', is_flag=True, callback=utils.enable_verbose)
 @click.pass_context
 def init_konfigchanger(ctx, verbose):
     config_list_path = utils.get_config_list_path()
     store_dir = utils.get_store_dir()
     error_code, error = utils.create_directory(store_dir)
+    # TODO: copy default "locations" file to .config/konfigchanger
     if error_code == -1:
         utils.logger.info('Backup folder already exists at ' + store_dir)
         utils.logger.info('You are good to go. Dont need to run init command again')
     elif error_code == 1:
         utils.logger.info('Could not create directory for config backup at ' + store_dir)
-        utils.logger.info(e)
+        utils.logger.info(error)
+        return 1
     else:
         utils.logger.log('Backup folder created successfully')
+    return 0
 
 
 @konfchanger.command()
-@click.option('-v', '--verbose', is_flag=True,  callback=utils.enable_verbose)
+@click.option('-v', '--verbose', is_flag=True, callback=utils.enable_verbose)
 @click.option('--name')
+# TODO: Check --name flag
+# TODO: implement overwrite_existing flag
+# TODO: implement post backup hook flag
 @click.pass_context
 def backup(ctx, name, verbose):
-    '''Backup current configuration'''
+    """Backup current configuration"""
 
     if not utils.is_config_list_path_present():
         ctx.abort()
     if name is None:
-        name = click.prompt('Please give a name to the current configuration backup!\nNOTE: <Spaces> in the name will be converted into "_"(underscore)', type=click.STRING)
-    fixed_name = name.replace(' ', '_')
+        name = click.prompt(
+            'Please give a name to the current configuration backup! \nNOTE:Name starting with "." will be eliminated',
+            type=click.STRING)
+    fixed_name = name.strip()
+    if fixed_name.startswith('.'):
+        fixed_name = fixed_name[1:]
     configuration_exists = utils.is_duplicate_name_present(fixed_name)
     absolute_path = utils.get_config_backup_absolute_path_by_name(fixed_name)
     overwrite = False
     if configuration_exists:
         overwrite = click.confirm('Do you want to overwrite the exisiting configuration backup?', abort=True)
-        #utils.delete_location(absolute_path)
+        # utils.delete_location(absolute_path)
         utils.logger.log(str(overwrite))
         if not overwrite:
             ctx.abort()
@@ -72,20 +85,23 @@ def backup(ctx, name, verbose):
             utils.logger.info('Some error occurred while backing up your configurations.')
             utils.logger.info('Please use the delete command to delete this configurations backup if needed')
         else:
-            utils.logger.info(name + ' Backup complete, You can apply this configuration by passing this name -> ' + fixed_name + ' with the --name flag for "apply" option')
+            utils.logger.info(
+                name + ' Backup complete, You can apply this configuration by passing this name -> ' + fixed_name + ' with the --name flag for "apply" option')
+            return 0
     elif error_code == 1:
         utils.logger.info('Error creating backup folder at ' + absolute_path)
         utils.logger.info(error)
-
-
+        return 1
 
 
 @konfchanger.command()
 @click.option('-v', '--verbose', is_flag=True, callback=utils.enable_verbose)
 @click.option('--name')
+# TODO: check name flag
+# TODO: implement post apply hook flag
 @click.pass_context
 def apply(ctx, name, verbose):
-    '''Apply a backed-up configuration'''
+    """Apply a backed-up configuration"""
 
     stored_configs = utils.get_stored_configs()
     if stored_configs is None:
@@ -96,47 +112,53 @@ def apply(ctx, name, verbose):
     if len(stored_configs) == 1:
         utils.logger.info('Only 1 configuration pack found!!')
         name = stored_configs[0]
-        if not click.confirm('Do you want to apply '+ name + ' configuration pack?'):
+        if not click.confirm('Do you want to apply ' + name + ' configuration pack?'):
             ctx.abort()
+            return 1
     else:
-        if (name is not None) and (name not in stored_configs):# if wrong name is provided
-            utils.logger.info(name + ' provided name doesnt match with any existing stored configurations.\n Please select one from below:\n')
-        if (name is None) or (name not in stored_configs): #if no name is provided or wrong name is provided
+        if (name is not None) and (name not in stored_configs):  # if wrong name is provided
+            utils.logger.info(
+                name + ' provided name doesnt match with any existing stored configurations.\n Please select one from below:\n')
+        if (name is None) or (name not in stored_configs):  # if no name is provided or wrong name is provided
             name = utils.get_config_name()
     utils.create_bak_file(ctx)
     utils.copy_to_set_locations(ctx, name)
     # send kwin reconfigure signal
     utils.logger.info(name + ' ---- Applied')
-
+    return 0
 
 
 @konfchanger.command()
 @click.option('-v', '--verbose', is_flag=True, callback=utils.enable_verbose)
 @click.pass_context
 def list(ctx, verbose):
-    '''List all available backed up configurations'''
+    """List all available backed up configurations"""
     utils.logger.log('Listing existing configurations')
     utils.get_stored_configs()
     utils.echo_configs()
+    return 0
 
 
-#@click.option('-y','--yes')
+# @click.option('-y','--yes')
 @konfchanger.command('delete')
 @click.option('-n', '--name')
-@click.option('-v', '--verbose', is_flag=True,  callback=utils.enable_verbose)
+# TODO: check name flag
+@click.option('-v', '--verbose', is_flag=True, callback=utils.enable_verbose)
 @click.pass_context
 def delete_configuration_backup(ctx, name, verbose):
-    '''Delete a backed-up configuration'''
+    """Delete a backed-up configuration"""
 
     stored_configs = utils.get_stored_configs()
     if stored_configs is None:
         utils.logger.info('No backed up configuration packs present!!\nBackup folder is empty')
-        return
-    if (name is not None) and (name not in stored_configs): # if wrong name is provided
-        utils.logger.info(name + ' provided name doesnt match with any existing saved configurations.\n Please select 1 from below:\n')
-    if (name is None) or (name not in stored_configs): # if no name is provided or wrong name is provided
+        return 1
+    if (name is not None) and (name not in stored_configs):  # if wrong name is provided
+        utils.logger.info(
+            name + ' provided name doesnt match with any existing saved configurations.\n Please select 1 from below:\n')
+    if (name is None) or (name not in stored_configs):  # if no name is provided or wrong name is provided
         name = utils.get_config_name()
-    if click.confirm('Do you really want to delete '+ name + ' configuration?', abort=True):
+    if click.confirm('Do you really want to delete ' + name + ' configuration?', abort=True):
         rem_config_path = utils.get_config_backup_absolute_path_by_name(name)
         utils.delete_location(rem_config_path)
         utils.logger.info(name + ' configuration deleted!!')
+        return 0
--- konfchanger_utils.py
+++ konfchanger_utils.py
@@ -20,8 +20,8 @@ BAK_FILE_EXTENSION = '.bak'
 class Utils:
 
     def __init__(self):
-        self.__info_map = my_dict()
-        self.logger = my_dict()
+        self.__info_map = My_Dict()
+        self.logger = My_Dict()
 
         self.__set_info_logger()
         self.__set_home_dir()
@@ -33,23 +33,18 @@ class Utils:
         self.__load_konfigchanger_config()
         pass
 
-
     def __set_info_logger(self):
         self.logger.info = click.echo
-
 
     def __set_home_dir(self):
         self.__info_map.home_dir = os.getenv('HOME')
 
-
     def __set_current_directory(self):
         self.__info_map.current_dir = os.getcwd()
-
 
     def __set_konfigchanger_config(self):
         current_path = self.get_current_directory()
         self.__info_map.konfigchanger_config = os.path.join(current_path, '.konfchanger_default_config')
-
 
     def __load_konfigchanger_config(self):
         konfigchanger_config = self.get_value('konfigchanger_config')
@@ -58,28 +53,25 @@ class Utils:
             self.__info_map['store_dir'] = os.path.join(self.get_home_path(), json_data['store_dir'])
             self.__info_map['config_list_path'] = json_data['config_list_path']
 
-
     def __set_stored_config_list(self, stored_configs):
         self.__info_map.store_config_list = stored_configs
 
-
     def __set_verbose(self, verbose):
-        '''Set verbose function to print verbose statements if verbose flag is set'''
+        """Set verbose function to print verbose statements if verbose flag is set"""
+
         def identity(*args, **args1):
             pass
+
         self.logger.log = click.echo if verbose else identity
 
     def enable_verbose(self, ctx, flag_name, enable_flag=False):
         self.__set_verbose(enable_flag)
 
-
     def get_home_path(self):
         return self.get_value('home_dir')
 
-
     def get_current_directory(self):
         return self.get_value('current_dir')
-
 
     def get_konfigchanger_config(self):
         return self.get_value('konfigchanger_config')
@@ -90,26 +82,27 @@ class Utils:
     def is_konfigchanger_config_present(self):
         path = self.get_value('konfigchanger_config')
         if not os.path.isfile(path):
-            self.logger.info('.konfchanger_default_config file doesnt not exist at '+path)
-            self.logger.info('please create a .konfchanger_default_config file in this directory:'+ self.get_current_directory())
-            self.logger.info('It should contain the following \{\n'+
-                                '"store_dir":"<full_path_to_a_location_to_store_backups>",'+
-                                '"config_list_path":"<path_to_a_file_containing_list_of_config_files_to_backup>"'+
-                                '\}')
+            self.logger.info('.konfchanger_default_config file doesnt not exist at ' + path)
+            self.logger.info(
+                'please create a .konfchanger_default_config file in this directory:' + self.get_current_directory())
+            self.logger.info('It should contain the following \{\n' +
+                             '"store_dir":"<full_path_to_a_location_to_store_backups>",' +
+                             '"config_list_path":"<path_to_a_file_containing_list_of_config_files_to_backup>"' +
+                             '\}')
             return False
         else:
             self.logger.log('Config file Found!!')
             return True
-
 
     def is_config_list_path_present(self):
         '''Check if the file containing list to other configuration file to be backed up is present or not'''
 
         path = self.get_value('config_list_path')
         if not os.path.isfile(path):
-            self.logger.info('Configuration List providing file not present at '+path)
+            self.logger.info('Configuration List providing file not present at ' + path)
             self.logger.info('Create a file at the above location with following contents:')
-            self.logger.info('<relative_to_home_path_to_config_file_1>\n<relative_to_home_path_to_config_file_2>\n:\n<relative_to_home_path_to_config_file_n>')
+            self.logger.info(
+                '<relative_to_home_path_to_config_file_1>\n<relative_to_home_path_to_config_file_2>\n:\n<relative_to_home_path_to_config_file_n>')
             self.logger.info('any location which starts with a "#" will be ignored')
             return False
         else:
@@ -117,7 +110,7 @@ class Utils:
             return True
 
     def is_store_dir_present(self):
-        '''Checks if store directory is present or not'''
+        """Checks if store directory is present or not"""
 
         store_dir = self.get_value('store_dir')
         if not os.path.isdir(store_dir):
@@ -127,26 +120,23 @@ class Utils:
             self.logger.log('Found Backup directory')
             return True
 
-
     def is_duplicate_name_present(self, name):
-        name_path =  os.path.join(self.get_store_dir(), name)
+        name_path = os.path.join(self.get_store_dir(), name)
         if os.path.isdir(name_path):
             self.logger.info('configuration backup with this name already exists')
             return True
         else:
             return False
 
-
     def get_value(self, key):
-        '''Get value from info_map in the object'''
+        """Get value from info_map in the object"""
 
         if key in self.__info_map.keys():
             return self.__info_map[key]
         return None
 
-
     def get_stored_configs(self):
-        '''Gets the list of stored configurations from store folder'''
+        """Gets the list of stored configurations from store folder"""
 
         store_dir = self.get_value('store_dir')
         stored_configs = [config for config in os.listdir(store_dir)]
@@ -164,7 +154,7 @@ class Utils:
         return self.get_value('store_dir')
 
     def get_config_name(self):
-        '''Gives user the list of stored configs prvided in parameter and lets them choose one from the list'''
+        """Gives user the list of stored configs prvided in parameter and lets them choose one from the list"""
 
         self.echo_configs()
         stored_configs = self.get_value('store_config_list')
@@ -172,13 +162,13 @@ class Utils:
             self.logger.info('No backed up configuration packs present!!\nBackup folder is empty')
             return
         no_configs = len(stored_configs)
-        value = click.prompt('Please enter the number associated with the configuration, you want to choose:', type=click.IntRange(1, no_configs))
+        value = click.prompt('Please enter the number associated with the configuration, you want to choose:',
+                             type=click.IntRange(1, no_configs))
         name = stored_configs[value - 1]
         return name
 
-
     def echo_configs(self):
-        '''Prints the stored config list passed'''
+        """Prints the stored config list passed"""
 
         stored_configs = self.get_value('store_config_list')
         if stored_configs is None:
@@ -186,11 +176,11 @@ class Utils:
             return
         self.logger.info('These are the stored configs:')
         no_configs = len(stored_configs)
-        for i in range(1,no_configs+1):
-            self.logger.info('['+str(i)+'] '+stored_configs[i-1])
+        for i in range(1, no_configs + 1):
+            self.logger.info('[' + str(i) + '] ' + stored_configs[i - 1])
 
     def __get_source_paths(self):
-        '''Get the list of configuration source paths from where we have to backup/copy configurations'''
+        """Get the list of configuration source paths from where we have to backup/copy configurations"""
 
         home_path = self.get_home_path()
         source_paths_file_location = self.get_value('config_list_path')
@@ -198,7 +188,7 @@ class Utils:
         with open(source_paths_file_location, 'r') as source_paths_file:
             for source_path in source_paths_file:
                 source_path = source_path.strip('\n')
-                source_path = source_path.replace(' ','\ ')
+                source_path = source_path.replace(' ', '\ ')
                 if (len(source_path) >= 2) & (not source_path.startswith('#')):
                     source_path = os.path.join(home_path, source_path)
                     source_paths.append(source_path)
@@ -206,7 +196,7 @@ class Utils:
         return source_paths
 
     def copy_configs_to_store(self, dest):
-        '''Copy the current configurations mentioned into a store-configuration folder'''
+        """Copy the current configurations mentioned into a store-configuration folder"""
 
         source_path_list = self.__get_source_paths()
         error_occurred = False
@@ -219,21 +209,20 @@ class Utils:
                 error_occurred = True
         return error_occurred
 
-
     def __bak_file_exists(self, source_paths):
-        '''Checks if the backup exists for the source paths provided'''
+        """Checks if the backup exists for the source paths provided"""
 
         no_bk_list = list()
         for source_path in source_paths:
             source_path_bk = source_path + BAK_FILE_EXTENSION
-            self.logger.log('checking for backup config '+ source_path_bk)
+            self.logger.log('checking for backup config ' + source_path_bk)
             exists = os.path.exists(source_path_bk)
             if not exists:
                 no_bk_list.append(source_path)
         return no_bk_list
 
     def create_bak_file(self, ctx):
-        '''Creates backup for the current source configurations for which no backup exists'''
+        """Creates backup for the current source configurations for which no backup exists"""
 
         source_paths = self.__get_source_paths()
         no_bk_list = self.__bak_file_exists(source_paths)
@@ -243,17 +232,17 @@ class Utils:
         any_error = False
         for source_path in no_bk_list:
             source_path_bk = source_path + BAK_FILE_EXTENSION
-            self.logger.log('creating backup for '+source_path)
+            self.logger.log('creating backup for ' + source_path)
             exit_code = call(['mv', '-v', source_path, source_path_bk])
             if exit_code != 0:
-                self.logger.info('Could not backup ' + source_path + ' to '+source_path_bk)
+                self.logger.info('Could not backup ' + source_path + ' to ' + source_path_bk)
                 any_error = True
         if any_error:
             self.logger.info('There was error creating backup for 1 or more configurations\nSo aborting....')
             ctx.abort()
 
     def copy_to_set_locations(self, ctx, stored_config_name):
-        '''Copy the stored configuration to the specific locations'''
+        """Copy the stored configuration to the specific locations"""
 
         default_locations = self.__get_source_paths()
         store_dir = self.get_value('store_dir')
@@ -263,19 +252,18 @@ class Utils:
         for config in src:
             associated_path = self.__get_associated_path(config, default_locations)
             if associated_path is None:
-                slef.logger.log('could not find associated path to apply '+config)
+                self.logger.log('could not find associated path to apply ' + config)
                 self.logger.log('So skipping applying this config!!')
                 continue
             source_location = os.path.join(source_path, config)
-            self.logger.log('Applying '+source_location + ' to '+associated_path)
+            self.logger.log('Applying ' + source_location + ' to ' + associated_path)
             error_code = call(['cp', '-a', source_location, associated_path])
             if error_code != 0:
                 any_error = True
-                self.logger.info('Issue copying '+source_location + ' into '+associated_path)
+                self.logger.info('Issue copying ' + source_location + ' into ' + associated_path)
         if any_error:
             self.logger.info('Encountered error while applying 1 or more configurations....\nSo aborting')
             ctx.abort()
-
 
     def __get_associated_path(self, config, config_paths):
         for config_path in config_paths:
@@ -286,10 +274,10 @@ class Utils:
         shutil.rmtree(location)
 
     def create_directory(self, location, overwrite=False):
-        '''Creates directory at said location:
-            return 1: if error occured while creating directory
+        """Creates directory at said location:
+            return 1: if error occurred while creating directory
                    0: if directory created successfully
-                  -1: if directory already exists and overwrite flag was False'''
+                  -1: if directory already exists and overwrite flag was False"""
 
         if os.path.exists(location):
             self.logger.log('Folder at ' + location + ' already exists')
@@ -303,7 +291,9 @@ class Utils:
         except Exception as error:
             return 1, error
 
-class my_dict(dict):
-    '''My own dictionary implementation to use dictionary both as a dict and like an object interchengably'''
+
+class My_Dict(dict):
+    """My own dictionary implementation to use dictionary both as a dict and like an object interchangeably"""
+
     def __init__(self):
         self.__dict__ = self
